2c238562bcfa6469de8a01407f59976c2cbc82e2
Nick LaMuro <nicklamuro@gmail.com>
2017-12-11 17:58:46 -0600

    [miq_server_leak_discovery] Fix GC.start in loop
    In the previous iteration of this code, the `GC.start && do_gc = false`
    was never hitting the second part of the code since `GC.start` always
    returns `nil`.
    
    This change fixes that and also makes the if statements more defined by
    adding some whitespace.

--- miq_server_leak_discovery/03_heartbeat_loop_test.rb
+++ miq_server_leak_discovery/03_heartbeat_loop_test.rb
@@ -10,7 +10,7 @@ server = MiqServer.my_server
 puts "starting loop..."
 loop do
   server.heartbeat
-  GC.start && do_gc = false if do_gc  && Time.now.min % 2 == 0
-  do_gc = true              if !do_gc && Time.now.min % 2 != 0
+  (GC.start; do_gc = false)   if do_gc  && Time.now.min % 2 == 0
+  do_gc = true                if !do_gc && Time.now.min % 2 != 0
   sleep 0.5
 end
