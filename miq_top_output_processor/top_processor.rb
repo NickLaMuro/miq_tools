7c59023bfd858a832a9ff0e287cbe7e8fb723800
Nick LaMuro <nicklamuro@gmail.com>
2018-01-18 19:18:56 -0600

    [top_processor] Use DateStringStruct from util

--- miq_top_output_processor/top_processor.rb
+++ miq_top_output_processor/top_processor.rb
@@ -99,65 +99,19 @@ TOP_PROC_LINE_REGEXP = Regexp.new [
 # determine the current date.
 TIMESYNC_REGEXP = /timesync: date time is-> (.*)$/
 
-class DateStruct
-  attr_reader :date
+$: << File.expand_path(File.join("..", "..", "util"), __FILE__)
+require 'byte_formatter'
+require 'time'
+require 'bigdecimal'
+require 'date_string_struct'
 
-  class << self
-    attr_accessor :tz_offset
-  end
-
-  def initialize(datetime_str)
-    if datetime_str
-      @datetime      = parse_time(datetime_str) - self.class.tz_offset
-
-      @date          = @datetime.strftime("%Y-%m-%d")
-      @date_1_hr_ago = (@datetime - 60*60).strftime("%Y-%m-%d")
-      @date_add_1_hr = (@datetime + 60*60).strftime("%Y-%m-%d")
-    end
-  end
-
-  # Checks the given timestamp to see if it should fall before, after, or on
-  # the date in the struct
-  #
-  # timestamps given here should always be within 1 hour of the date when
-  # initialized, so if it is not either hours 00 or 23, then we can just assume
-  # it is the same date.
-  def set_for_time timestamp
-    return nil unless @date
-    case timestamp[0,2]
-    when "00"
-      @datetime.hour == 23 ? @date_add_1_hr : @date
-    when "23"
-      @datetime.hour == 00 ? @date_1_hr_ago : @date
-    else
-      @date
-    end
-  end
-
-  private
-
-  # In ruby 2.2, there was a change to Time.parse where the current timezone of
-  # the host computer was no longer interpreted when calling `Time.parse`.
-  # These lines define methods so that the time parsing is consistent across
-  # ruby versions, and uses the old implementation as the common denominator.
-  if RbConfig::CONFIG["MAJOR"] == 2 && RbConfig::CONFIG["MAJOR"] > 1
-    def parse_time(time); Time.parse(time).localtime; end
-  else
-    def parse_time(time); Time.parse(time); end
-  end
-end
-
-DateStruct.tz_offset = options[:offset]
+DateStringStruct.tz_offset = options[:offset]
 
 data_file   = {}
 pid_buffer  = {}
-date        = DateStruct.new(nil)
+date        = DateStringStruct.new(nil)
 
 current_pid, time, cpu1, cpu5, cpu15 = nil
-
-$: << File.expand_path(File.join("..", "..", "util"), __FILE__)
-require 'time'
-require 'bigdecimal'
 
 Dir.mkdir "top_outputs" unless Dir.exists? "top_outputs"
 
@@ -230,7 +184,7 @@ log_files.each do |log_file|
         end if pid_buffer[current_pid]
 
       when TIMESYNC_REGEXP
-        date = DateStruct.new Regexp.last_match[1]
+        date = DateStringStruct.new Regexp.last_match[1]
 
         (pid_buffer[current_pid] || []).each do |entry|
           puts entry unless entry[:time]
